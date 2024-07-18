59554214d9e6313ea8360dbf5942a5444def661d
gannemjahnavi <105412455+gannemjahnavi@users.noreply.github.com>
2024-07-18 14:33:10 +0530

    Update union of two sorted arrays.py

--- union of two sorted arrays.py
+++ union of two sorted arrays.py
@@ -1,7 +1,6 @@
 a=[1,2,3,4,11,12]
 b=[1,2,9,5,6,7,8]
 c=[]
-d=[]
 def union():
     for i in a:
         for j in b:
@@ -9,11 +8,10 @@ def union():
                 c.append(i)
     for i in a:
         if not i in b:
-            d.append(i)
+            c.append(i)
     for j in b:
         if not j in a:
-            d.append(j)
+            c.append(j)
     c.sort()
-    d.sort()
-    return c+d
+    return c
 print(union())
