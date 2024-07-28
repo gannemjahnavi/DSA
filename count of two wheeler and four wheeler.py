c2e460d461320c0ab887b34c3b0604381f203a02
gannemjahnavi <105412455+gannemjahnavi@users.noreply.github.com>
2024-07-28 21:58:32 +0530

    Update count of two wheeler and four wheeler.py

--- count of two wheeler and four wheeler.py
+++ count of two wheeler and four wheeler.py
@@ -1,17 +1,17 @@
 def twfw():
-    v = int(input("Enter the total number of vehicles: "))
-    w = int(input("Enter the total number of wheels: "))
+    v = int(input())
+    w = int(input())
     
     if w % 2 != 0 or w < 2:
         return 0
     
-    for tw in range(v + 1):
-        fw = v - tw
-        if (2 * tw + 4 * fw) == w:
-            print(f"TW = {tw}, FW = {fw}")
+    for i in range(v + 1):
+        if i + (v - i) == v and (i * 2 + (v - i) * 4 == w or i * 4 + (v - i) * 2 == w):
+            print(f"TW = {i}, FW = {v - i}")
             return 0
     
     print("No solution found")
     return 0
 
 twfw()
+
